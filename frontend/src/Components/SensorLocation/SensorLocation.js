a6941a6661c00128a45e6fd5e51a5e60ae24efac
Lea <lea.levy406@gmail.com>
2019-12-10 23:23:46 +0100

    Location sensor done

--- frontend/src/Components/SensorLocation/SensorLocation.js
+++ frontend/src/Components/SensorLocation/SensorLocation.js
@@ -8,7 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const axios = require('axios');
 
 class SensorLocation extends React.Component {
-/*
+
   //Permet de set les données lors de la construction du component 
   componentDidMount() {
     this.getData();
@@ -17,27 +17,45 @@ class SensorLocation extends React.Component {
   constructor(props) {
     super(props); 
     this.state = {
-      sensorLocation:'', 
+      //sensorLocation:'',
+      sensorLocation:[
+          "France" 
+      ]
     }
   }
 
+
   getData() {
     //Permet de get la data from  la BDD
-      axios.get(`http://localhost:3001/sensors/${this.props.userId}`)
+      axios.get(`http://localhost:3001/sensors/`)
       .then(response => {
-          this.setState({ sensorLocation: response.data.location });
-      })
-      .catch(error =>{
+        let sensorUser= []; 
+        let buffer = []; 
+        
+        //this.setState({ dataSensor: response.data });
+        //response.data.forEach(element => sensorUser = element.filter    )
+        sensorUser = response.data.filter(element => (element.userID === this.props.userId)  );
+        
+        sensorUser.forEach(element => buffer.push(element.location) ); 
+        //console.log( sensorUser); 
+
+        this.setState({ sensorLocation: buffer.slice() });
+
+        })
+        .catch(error =>{
           console.log("Inside error");
           console.log(error);
       })    
-  }*/
+  }
 
     render(){
       return(
         <Col sm={12}  md={3} className="Col rounded" >
           <h4> Sensor location  </h4>
-          <p>  </p> {/*{this.state.location} */}
+           
+           {this.state.sensorLocation.map((element) => ( <p> {element} </p>  ))}
+           
+           
         </Col>
         
       ); 
